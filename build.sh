91b0f794030823794bb02105f175491fb0264a6c
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-10-23 14:19:10 -0500

    fix: :bug: Try uninstalling plugins with dnf

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # Klassy
-# rpm-ostree install dnf-plugins-core
+rpm-ostree install dnf-plugins-core
 dnf-3 config-manager --add-repo https://download.opensuse.org/repositories/home:paul4us/Fedora_40/home:paul4us.repo
 rpm-ostree install klassy
 
@@ -22,7 +22,7 @@ rpm-ostree install mtr neovim syncthing
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
-# rpm-ostree remove dnf-plugins-core
+dnf remove dnf-plugins-core
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
