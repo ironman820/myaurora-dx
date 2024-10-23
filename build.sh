70e03b7c88787cc40ab4b3cd4210fed4d7c313a5
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-10-23 13:04:29 -0500

    Install Klassy

--- build.sh
+++ build.sh
@@ -4,13 +4,16 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+
+# Klassy
+rpm-ostree config-manager --add-repo https://download.opensuse.org/repositories/home:paul4us/Fedora_40/home:paul4us.repo
+rpm-ostree install klassy
 
 # this installs a package from fedora repos
 rpm-ostree install mtr neovim syncthing
