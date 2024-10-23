2f0f76f951e8265a1a2505e242f886aa9b8b22ec
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2024-10-23 14:29:20 -0500

    Install Klassy (#4)
    * Install Klassy
    
    * fix: :bug: dnf to add repo
    
    * fix: :bug: Install dnf-plugins before adding repo
    
    * fix: :bug: Attempt consistent commands
    
    * fix: :bug: Try dnf-3
    
    * fix: :bug: Try config-manager without extra install/remove step
    
    * fix: :bug: Try uninstalling plugins with dnf
    
    * fix: :bug: Assume yes during dnf uninstall

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -12,12 +11,18 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# Klassy
+rpm-ostree install dnf-plugins-core
+dnf-3 config-manager --add-repo https://download.opensuse.org/repositories/home:paul4us/Fedora_40/home:paul4us.repo
+rpm-ostree install klassy
+
 # this installs a package from fedora repos
 rpm-ostree install mtr neovim syncthing
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
+dnf remove -y dnf-plugins-core
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
