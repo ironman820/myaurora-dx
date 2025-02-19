5ff735e582a16d46256b4bac0ffc5e98394f2572
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2025-02-19 14:47:18 -0600

    Setup yEd diagram editor

--- build.sh
+++ build.sh
@@ -26,6 +26,10 @@ rpm-ostree install mtr neovim syncthing
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
+wget -O /tmp/yEd-3.25-setup.sh "https://www.yworks.com/resources/yed/demo/yEd-3.25_with-JRE23_64-bit_setup.sh"
+chmod +x /tmp/yEd-3.25-setup.sh
+/tmp/yEd-3.25-setup.sh -varfile /tmp/response.varfile
+
 dnf5 remove -y dnf-plugins-core
 #### Example for enabling a System Unit File
 
